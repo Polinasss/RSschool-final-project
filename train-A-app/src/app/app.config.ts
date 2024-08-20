aaaef18ca41a83d6f5b9ebe566bdd5ed9b0bb832
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-20 11:49:03 +0200

    fix: format issues

--- train-A-app/src/app/app.config.ts
+++ train-A-app/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
