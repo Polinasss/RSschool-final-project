7991e6ee75b5aa556a160a555d689640c65fc80c
Vitali Starastsenka <vstarastsenka@ibagroup.eu>
2024-08-21 20:23:16 +0300

    feat:signup component and page

--- train-A-app/src/app/app.routes.ts
+++ train-A-app/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-import { SigninComponent } from './login/pages/login/signin.component';
-import { SigninGuard } from './login/guards/signin.guard';
 import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
+import { SigninPageComponent } from './auth/pages/signin-page/signin-page.component';
 
 export const routes: Routes = [
-  {
-    path: 'signin',
-    component: SigninComponent,
-    canActivate: [SigninGuard],
-  },
   { path: 'signup', component: SignupPageComponent },
+  { path: 'signin', component: SigninPageComponent },
 ];
