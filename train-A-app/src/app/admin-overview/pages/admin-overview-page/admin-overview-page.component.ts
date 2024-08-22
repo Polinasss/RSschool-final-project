e0da1b3022b7f7ea2510cb5589a50070099432e7
Melnychuk <imelnychuk@luxoft.com>
2024-08-23 01:39:11 +0200

    feat: implement Stations Page component

--- train-A-app/src/app/admin-overview/pages/admin-overview-page/admin-overview-page.component.ts
+++ train-A-app/src/app/admin-overview/pages/admin-overview-page/admin-overview-page.component.ts
@@ -2,18 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { StationsPageComponent } from '../stations-page/stations-page.component';
 
 @Component({
   selector: 'app-admin-overview-page',
   standalone: true,
-  imports: [MatDividerModule, MatListModule, CommonModule],
+  imports: [MatDividerModule, MatListModule, CommonModule, StationsPageComponent],
   templateUrl: './admin-overview-page.component.html',
   styleUrl: './admin-overview-page.component.scss',
 })
 export class AdminOverviewPageComponent {
   selectedPanelItem: string = 'Stations';
 
-  selectPanelItem(panelItem: string) {
+  selectPanelItem(panelItem: string): void {
     this.selectedPanelItem = panelItem;
   }
 }
---
+++ train-A-app/src/app/admin-overview/pages/stations-page/stations-page.component.ts
@@ -0,0 +1,87 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { StationMapComponent } from '../../components/station-map/station-map.component';
+import { StationFormComponent } from '../../components/station-form/station-form.component';
+import { StationListComponent } from '../../components/station-list/station-list.component';
+import { LocationData, Station } from '../../models/station';
+import { MOCK_STATIONS } from '../../models/mocked-data';
+
+@Component({
+  selector: 'app-stations-page',
+  standalone: true,
+  imports: [
+    StationMapComponent,
+    StationFormComponent,
+    StationListComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './stations-page.component.html',
+  styleUrl: './stations-page.component.scss',
+})
+export class StationsPageComponent {
+  stationsConnectedForm: FormGroup;
+
+  selectedLocation: LocationData | null = null;
+
+  stations: Station[] = MOCK_STATIONS;
+
+  @ViewChild(StationMapComponent) mapComponent!: StationMapComponent;
+
+  @ViewChild(StationListComponent) listComponent!: StationListComponent;
+
+  constructor(private fb: FormBuilder) {
+    this.stationsConnectedForm = this.fb.group({
+      city: ['', Validators.required],
+      latitude: ['', [Validators.required, Validators.min(-90), Validators.max(90)]],
+      longitude: ['', [Validators.required, Validators.min(-180), Validators.max(180)]],
+      connectedTo: this.fb.array([]),
+    });
+  }
+
+  onLocationSelected(locationData: LocationData): void {
+    this.selectedLocation = locationData;
+    this.stationsConnectedForm.patchValue({
+      city: locationData.city,
+      latitude: locationData.latitude,
+      longitude: locationData.longitude,
+    });
+  }
+
+  onAddStation(newStation: Station) {
+    const stationExist = this.stations.find((station) => station.city === newStation.city);
+
+    if (stationExist) {
+      console.log('A station already exists in this city. Cannot add a new one!');
+      return;
+    }
+
+    this.stations.push(newStation);
+    this.mapComponent.updateMapMarkers();
+  }
+
+  onDeleteStation(stationId: number) {
+    const index = this.stations.findIndex((station) => station.id === stationId);
+    if (index > -1) {
+      const isConnected = this.stations.some((station) =>
+        station.connectedTo.some((connection) => connection.id === stationId.toString()),
+      );
+
+      if (isConnected) {
+        console.log('Cannot delete station with active rides!');
+        return;
+      }
+
+      this.stations.splice(index, 1);
+      if (this.mapComponent) {
+        this.mapComponent.updateMapMarkers();
+      }
+    }
+  }
+}
