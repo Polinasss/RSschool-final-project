93be063309632cdfa82f936957b320baaaff09f1
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-24 21:22:07 +0200

    fix: add admin page to routing

--- train-A-app/src/app/app.routes.ts
+++ train-A-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SigninGuard } from './auth/guards/signin.guard';
 import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
 import { SigninPageComponent } from './auth/pages/signin-page/signin-page.component';
+import { AdminOverviewPageComponent } from './admin-overview/pages/admin-overview-page/admin-overview-page.component';
 
 export const routes: Routes = [
   {
@@ -10,4 +11,8 @@ export const routes: Routes = [
     canActivate: [SigninGuard],
   },
   { path: 'signup', component: SignupPageComponent },
+  {
+    path: 'admin',
+    component: AdminOverviewPageComponent,
+  },
 ];
