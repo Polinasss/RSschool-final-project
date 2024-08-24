ac7cc4b779cedf74f2918f775b186c59f6e14f6b
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-24 21:07:35 +0200

    fix: add login to auth structure

--- train-A-app/src/app/auth/pages/signin-page/signin-page.component.ts
+++ train-A-app/src/app/auth/pages/signin-page/signin-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SigninComponent } from 'app/auth/components/signin/signin.component';
 
 @Component({
   selector: 'app-signin-page',
   standalone: true,
-  imports: [],
+  imports: [SigninComponent],
   templateUrl: './signin-page.component.html',
   styleUrl: './signin-page.component.scss',
 })
