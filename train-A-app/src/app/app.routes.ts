421feaeba84dc4ab69f650dd16764652ac887c05
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-24 21:07:35 +0200

    fix: add login to auth structure

--- train-A-app/src/app/app.routes.ts
+++ train-A-app/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { SigninComponent } from './login/pages/login/signin.component';
-import { SigninGuard } from './login/guards/signin.guard';
+import { SigninGuard } from './auth/guards/signin.guard';
 import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
+import { SigninPageComponent } from './auth/pages/signin-page/signin-page.component';
 
 export const routes: Routes = [
   {
     path: 'signin',
-    component: SigninComponent,
+    component: SigninPageComponent,
     canActivate: [SigninGuard],
   },
   { path: 'signup', component: SignupPageComponent },
