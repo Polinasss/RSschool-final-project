1a44acb1e529e747a2241c0e908f69ca49eddb73
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-28 15:09:29 +0200

    feat: add modal window during deleting route

--- train-A-app/src/app/admin-overview/components/route-item/route-item.component.ts
+++ train-A-app/src/app/admin-overview/components/route-item/route-item.component.ts
@@ -1,15 +1,24 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Route } from 'app/admin-overview/models/route';
 import { CapitalizeFirstLetterPipe } from 'app/shared/pipe/capitalize-first-letter/capitalize-first-letter.pipe';
+import { RouteDeleteComponent } from '../route-delete/route-delete.component';
 
 @Component({
   selector: 'app-route-item',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, NgFor, NgIf, CapitalizeFirstLetterPipe],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    NgFor,
+    NgIf,
+    CapitalizeFirstLetterPipe,
+    RouteDeleteComponent,
+  ],
   templateUrl: './route-item.component.html',
   styleUrl: './route-item.component.scss',
 })
@@ -18,9 +27,18 @@ export class RouteItemComponent {
 
   private router: Router = inject(Router);
 
+  readonly dialog = inject(MatDialog);
+
   public onEdit() {}
 
-  public onDelete() {}
+  public onDelete(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(RouteDeleteComponent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: { id: this.item.id },
+    });
+  }
 
   public assignRide() {
     this.router.navigate(['/admin/routes', this.item.id]);
