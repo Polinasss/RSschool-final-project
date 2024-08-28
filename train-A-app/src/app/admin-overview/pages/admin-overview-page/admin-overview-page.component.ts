ed91c153c710ad48df739ad083160aa0dacf4695
Melnychuk <imelnychuk@luxoft.com>
2024-08-28 18:29:08 +0200

    feat: implement loading stations from store

--- train-A-app/src/app/admin-overview/pages/admin-overview-page/admin-overview-page.component.ts
+++ train-A-app/src/app/admin-overview/pages/admin-overview-page/admin-overview-page.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { StationFacade } from 'app/admin-overview/_state/station/station.facade';
 import { StationsPageComponent } from '../stations-page/stations-page.component';
 
 @Component({
@@ -11,10 +12,16 @@ import { StationsPageComponent } from '../stations-page/stations-page.component'
   templateUrl: './admin-overview-page.component.html',
   styleUrl: './admin-overview-page.component.scss',
 })
-export class AdminOverviewPageComponent {
+export class AdminOverviewPageComponent implements OnInit {
   selectedPanelItem: string = 'Stations';
 
   selectPanelItem(panelItem: string): void {
     this.selectedPanelItem = panelItem;
+  }
+
+  private stationFacade = inject(StationFacade);
+
+  public ngOnInit(): void {
+    this.stationFacade.loadStation();
   }
 }
