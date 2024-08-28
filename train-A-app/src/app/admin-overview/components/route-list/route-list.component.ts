8590d39fe12fb2afd66c325ef056c793dfa9d9fb
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-29 00:23:09 +0200

    feat: add carriages names

--- train-A-app/src/app/admin-overview/components/route-list/route-list.component.ts
+++ train-A-app/src/app/admin-overview/components/route-list/route-list.component.ts
@@ -5,6 +5,9 @@ import { RouterOutlet } from '@angular/router';
 import { RouteFacade } from 'app/admin-overview/_state/route/route.facade';
 import { CreateButtonComponent } from 'app/shared/components/create-button/create-button.component';
 import { RoutePanelService } from 'app/admin-overview/services/route-panel.service';
+import { combineLatest, map, Observable } from 'rxjs';
+import { CarriageFacade } from 'app/admin-overview/_state/carriage/carriage.facade';
+import { Route } from 'app/admin-overview/models/route';
 import { RouteItemComponent } from '../route-item/route-item.component';
 import { RouteFormComponent } from '../route-form/route-form.component';
 import { RoutePanelComponent } from '../route-panel/route-panel.component';
@@ -30,9 +33,13 @@ import { RoutePanelComponent } from '../route-panel/route-panel.component';
 export class RouteListComponent implements OnInit {
   private routeFacade = inject(RouteFacade);
 
+  private carriageFacade = inject(CarriageFacade);
+
   private panelService = inject(RoutePanelService);
 
   readonly formState$ = this.panelService.panelState$;
+
+  readonly carriages$ = this.carriageFacade.carriage$;
 
   readonly routes$ = this.routeFacade.routes$;
 
@@ -40,8 +47,22 @@ export class RouteListComponent implements OnInit {
 
   readonly isLoading$ = this.routeFacade.isLoading$;
 
+  public routesWithStationsAndCarriages$!: Observable<{ route: Route; carriageName: string[] }[]>;
+
   public ngOnInit() {
     this.routeFacade.loadRoutes();
+    this.routesWithStationsAndCarriages$ = combineLatest([this.routes$, this.carriages$]).pipe(
+      map(([routes, carriages]) => {
+        return routes.map((route) => {
+          const routeCarriageCodes = route.carriages.map((carriage) => carriage);
+          const carriageName = carriages
+            .filter((carriage) => routeCarriageCodes.includes(carriage.code))
+            .map((carriage) => carriage.name);
+
+          return { route, carriageName };
+        });
+      }),
+    );
   }
 
   public openForm() {
