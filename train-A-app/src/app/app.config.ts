d53c5b7aa8706b6485e7c6664113ea488ad8abed
Maria Ushakova <m.ushakova22@gmail.com>
2024-08-29 17:42:30 +0200

    feat: clear form after saving

--- train-A-app/src/app/app.config.ts
+++ train-A-app/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideState, provideStore } from '@ngrx/store';
@@ -12,8 +11,6 @@ import { CarriageEffects } from './admin-overview/_state/carriage/carriage.effec
 import { authInterceptor } from './auth/auth.interceptor';
 import { stationFeature } from './admin-overview/_state/station/station.reducer';
 import { StationEffects } from './admin-overview/_state/station/station.effects';
-import { RoutesEffects } from './admin-overview/_state/route/route.effects';
-import { routeFeature } from './admin-overview/_state/route/route.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
