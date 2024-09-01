f91671dceb8efadeb0e6b00d4d21e43c65dbedc6
Melnychuk <imelnychuk@luxoft.com>
2024-09-02 00:40:21 +0200

    feat: implement notification (SnackBar) for successful operations

--- train-A-app/src/app/core/services/notification/notification.service.ts
+++ train-A-app/src/app/core/services/notification/notification.service.ts
@@ -14,4 +14,12 @@ export class NotificationService {
       panelClass: ['red-snackbar'],
     });
   }
+
+  openSuccessSnackBar(message: string) {
+    this.snackBar.open(message, 'OK', {
+      duration: 3000,
+      verticalPosition: 'top',
+      panelClass: ['green-snackbar'],
+    });
+  }
 }
