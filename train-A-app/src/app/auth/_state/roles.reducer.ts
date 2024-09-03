2851386ab62146caf1eddc6aa574885c088bafe1
Polinasss <polina129889@gmail.com>
2024-09-03 16:06:55 +0300

    fix: delete service in layout

--- train-A-app/src/app/auth/_state/roles.reducer.ts
+++ train-A-app/src/app/auth/_state/roles.reducer.ts
@@ -1,7 +1,7 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { rolesListActions } from './roles.actions';
 
-export const initialState: string = 'null';
+export const initialState: string = 'guest';
 
 export const rolesReducer = createFeature({
   name: 'roleState',
