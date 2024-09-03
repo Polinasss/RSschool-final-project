361461929e056546c88b8d01660f4f90e98ce8f2
SheldonGomel <sheldongomel@gmail.com>
2024-09-03 17:12:03 +0300

    feat: add carriages item component

--- train-A-app/src/app/trip/components/trip/trip.component.ts
+++ train-A-app/src/app/trip/components/trip/trip.component.ts
@@ -12,10 +12,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CarriagesItemComponent } from 'app/admin-overview/components/carriages-item/carriages-item.component';
+import { CarriageFacade } from 'app/admin-overview/_state/carriage/carriage.facade';
 
 @Component({
   standalone: true,
-  imports: [MatTabsModule, DatePipe, MatIcon, MatButtonModule],
+  imports: [MatTabsModule, DatePipe, MatIcon, MatButtonModule, CarriagesItemComponent],
   selector: 'app-trip',
   templateUrl: './trip.component.html',
   styleUrls: ['./trip.component.scss'],
@@ -63,96 +65,100 @@ export class TripComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private tripService: TripService,
     private tripFacade: TripFacade,
+    private carriageFacade: CarriageFacade,
     private dialog: MatDialog,
     private router: Router,
   ) {
-    // this.tripFacade.saveRide({
-    //   routeId: 2,
-    //   fromCity: 'city1',
-    //   toCity: 'city5',
-    //   stations: [
-    //     {
-    //       id: 1,
-    //       city: 'city1',
-    //     },
-    //     {
-    //       id: 4,
-    //       city: 'city4',
-    //     },
-    //     {
-    //       id: 63,
-    //       city: 'city63',
-    //     },
-    //     {
-    //       id: 5,
-    //       city: 'city5',
-    //     },
-    //   ],
-    //   carriages: [
-    //     {
-    //       type: 'carriage3',
-    //       price: 4985,
-    //       seats: 32,
-    //     },
-    //     {
-    //       type: 'carriage1',
-    //       price: 3684,
-    //       seats: 512,
-    //     },
-    //     {
-    //       type: 'carriage4',
-    //       price: 4197,
-    //       seats: 112,
-    //     },
-    //     {
-    //       type: 'carriage5',
-    //       price: 3477,
-    //       seats: 132,
-    //     },
-    //     {
-    //       type: 'carriage2',
-    //       price: 1608,
-    //       seats: 144,
-    //     },
-    //   ],
-    //   occupiedSeats: [],
-    //   date: new Date('2024-09-12T21:51:18.424Z'),
-    //   schedule: [
-    //     {
-    //       time: ['2024-09-03T15:53:18.424Z', '2024-09-06T00:17:18.424Z'],
-    //       price: {
-    //         carriage3: 1921,
-    //         carriage1: 1523,
-    //         carriage4: 573,
-    //         carriage5: 2091,
-    //         carriage2: 876,
-    //       },
-    //       occupiedSeats: [],
-    //     },
-    //     {
-    //       time: ['2024-09-06T01:04:18.424Z', '2024-09-09T06:49:18.424Z'],
-    //       price: {
-    //         carriage3: 2268,
-    //         carriage1: 757,
-    //         carriage4: 1612,
-    //         carriage5: 994,
-    //         carriage2: 464,
-    //       },
-    //       occupiedSeats: [],
-    //     },
-    //     {
-    //       time: ['2024-09-09T07:46:18.424Z', '2024-09-12T21:51:18.424Z'],
-    //       price: {
-    //         carriage3: 796,
-    //         carriage1: 1404,
-    //         carriage4: 2012,
-    //         carriage5: 392,
-    //         carriage2: 268,
-    //       },
-    //       occupiedSeats: [],
-    //     },
-    //   ],
-    // });
+    this.carriageFacade.carriage$.subscribe((cs) => {
+      this.carriageTypes = cs;
+    });
+    this.tripFacade.saveRide({
+      routeId: 2,
+      fromCity: 'city1',
+      toCity: 'city5',
+      stations: [
+        {
+          id: 1,
+          city: 'city1',
+        },
+        {
+          id: 4,
+          city: 'city4',
+        },
+        {
+          id: 63,
+          city: 'city63',
+        },
+        {
+          id: 5,
+          city: 'city5',
+        },
+      ],
+      carriages: [
+        {
+          type: 'carriage3',
+          price: 4985,
+          seats: 32,
+        },
+        {
+          type: 'carriage1',
+          price: 3684,
+          seats: 512,
+        },
+        {
+          type: 'carriage4',
+          price: 4197,
+          seats: 112,
+        },
+        {
+          type: 'carriage5',
+          price: 3477,
+          seats: 132,
+        },
+        {
+          type: 'carriage2',
+          price: 1608,
+          seats: 144,
+        },
+      ],
+      occupiedSeats: [],
+      date: new Date('2024-09-12T21:51:18.424Z'),
+      schedule: [
+        {
+          time: ['2024-09-03T15:53:18.424Z', '2024-09-06T00:17:18.424Z'],
+          price: {
+            carriage3: 1921,
+            carriage1: 1523,
+            carriage4: 573,
+            carriage5: 2091,
+            carriage2: 876,
+          },
+          occupiedSeats: [],
+        },
+        {
+          time: ['2024-09-06T01:04:18.424Z', '2024-09-09T06:49:18.424Z'],
+          price: {
+            carriage3: 2268,
+            carriage1: 757,
+            carriage4: 1612,
+            carriage5: 994,
+            carriage2: 464,
+          },
+          occupiedSeats: [],
+        },
+        {
+          time: ['2024-09-09T07:46:18.424Z', '2024-09-12T21:51:18.424Z'],
+          price: {
+            carriage3: 796,
+            carriage1: 1404,
+            carriage4: 2012,
+            carriage5: 392,
+            carriage2: 268,
+          },
+          occupiedSeats: [],
+        },
+      ],
+    });
     this.tripFacade.ride$.subscribe((ride) => {
       this.ride = ride;
     });
@@ -169,6 +175,10 @@ export class TripComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  getCarriage(type: string) {
+    return this.carriageTypes.find((car) => car.name === type) ?? this.carriageTypes[0];
   }
 
   getCity(id: number) {
